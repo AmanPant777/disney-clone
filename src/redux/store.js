7a83bcda60fcab777649cee3d57c66ec3c45e9b8
AmanPant777 <footballeramanpt65@gmail.com>
2021-07-30 11:41:25 +0530

    make movieSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { getDefaultMiddleware } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import movieReducer from "./movieSlice";
 export default configureStore({
-  reducer: { user: userReducer },
+  reducer: {
+    user: userReducer,
+    movie: movieReducer,
+  },
   middleware: getDefaultMiddleware({
     serializableCheck: false,
   }),
