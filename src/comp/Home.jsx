0f70f2f5e0cb0961cfd52e7cdd1e276b79f380b7
AmanPant777 <footballeramanpt65@gmail.com>
2021-07-30 21:40:49 +0530

    add home useEffect for firebase data

--- src/comp/Home.jsx
+++ src/comp/Home.jsx
@@ -6,7 +6,47 @@ import Recommend from './Recommend'
 import NewDisney from './NewDisney'
 import Originals from './Originals'
 import Trending from './Trending'
+import { useDispatch } from 'react-redux'
+import { selectUserName } from '../redux/userSlice'
+import { setMovies } from '../redux/movieSlice'
+import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import db from '../firebase'
 const Home = () => {
+    const dispatch = useDispatch()
+    const username = useSelector(selectUserName)
+    let trending = [];
+    let recommends = [];
+    let newDisneys = [];
+    let originals = [];
+    useEffect(() => {
+        db.collection('movies').onSnapshot((snapshot) => {
+            snapshot.docs.map((doc) => {
+                switch (doc.data().type) {
+                    case 'recommend':
+                        recommends = [...recommends, { id: doc.id, ...doc.data() }];
+                        break;
+                    case 'new':
+                        newDisneys = [...newDisneys, { id: doc.id, ...doc.data() }];
+                        break;
+                    case 'trending':
+                        trending = [...trending, { id: doc.id, ...doc.data() }]
+                        break;
+                    case 'original':
+                        originals = [...originals, { id: doc.id, ...doc.data() }]
+                        break;
+                    default:
+                        break;
+                }
+            })
+            dispatch(setMovies({
+                recommend: recommends,
+                newDisney: newDisneys,
+                originals: originals,
+                trending: trending
+            }))
+        })
+    }, [username])
     return (
         <Container>
             <ImgSlider />
